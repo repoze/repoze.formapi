857e3f42d309c7cc249ab3f95088e23f0ed262b2
Stefan Eletzhofer <stefan.eletzhofer@inquant.de>
2008-10-15 19:36:00 +0000

    implemented converter. Added doctests. Removed unit tests.

--- src/repoze/formapi/converter.py
+++ src/repoze/formapi/converter.py
@@ -1,6 +1,49 @@
 
+def path_iterator(data):
+    """path_iterator(data) -> iterator
+
+    Return a ``iterator`` which iterates over all the paths
+    which in the data dict.
+
+        >>> data = dict(foo=dict(bar=42, baz=10), buz="hello")
+
+        >>> from repoze.formapi.converter import path_iterator
+        >>> sorted(list(path_iterator(data)))
+        ['buz', 'foo.bar', 'foo.baz']
+
+    """
+    for key, value in data.items():
+        if type(value) == dict:
+            for p in path_iterator(value):
+                yield "%s.%s" % (key, p)
+        else:
+            yield key
+
+
 def resolve_name(name, data):
-    """resolve the name given in the data given"""
+    """resolve the name given in the data given
+
+    Resolve a name in a dict::
+
+        >>> from repoze.formapi.converter import resolve_name
+        >>> data = dict(foo=dict(bar=42, baz=10), buz="hello")
+        >>> resolve_name("buz", data)
+        'hello'
+        >>> resolve_name("foo.bar", data)
+        42
+        >>> resolve_name("foo.baz", data)
+        10
+        >>> resolve_name("foo", data)
+        {'baz': 10, 'bar': 42}
+
+    A KeyError is raised for nonexisting keys::
+
+        >>> resolve_name("foo.notthere", data)
+        Traceback (most recent call last):
+        ...
+        KeyError: 'notthere'
+
+    """
 
     path = name.split(".")
 
@@ -11,29 +54,44 @@ def resolve_name(name, data):
     return obj
 
 
-def store_item(name, value, data, create_empty=False, container=None):
-    """store the named item in the data dict"""
+def store_item(name, value, data):
+    """Set items in a dict using a ``path``
+
+    Using ``store_item`` we're able to set values using a ``path``::
+
+        >>> from repoze.formapi.converter import store_item
+        >>> data = dict()
+        >>> store_item("user.name", "Fred Kaputnik", data)
+        >>> store_item("user.nick", "fred", data)
+        >>> store_item("user.age", 42, data)
+        >>> data
+        {'user': {'nick': 'fred', 'age': 42, 'name': 'Fred Kaputnik'}}
+
+    Note that ``empty`` path elements are created using new ``dict``
+    objects.
+
+    If a value exists, it is overwritten::
+
+        >>> store_item("user.name", "Alfred E. Neuman", data)
+        >>> data
+        {'user': {'nick': 'fred', 'age': 42, 'name': 'Alfred E. Neuman'}}
+
+    """
     path = name.split(".")
     path, key = path[:-1], path[-1]
 
     obj = data
     if len(path):
         for item in path:
-            if create_empty:
-                obj = obj.setdefault(item, dict())
-            else:
-                obj = obj[item]
+            obj = obj.setdefault(item, dict())
 
-    if obj.has_key(key):
-        if type(obj[key]) in (list, tuple):
-            obj[key] = container(list(obj[key]).append(value))
-        else:
-            obj[key] = container(list(obj[key]) + [value,])
+    old_value = obj.get(key)
+    if type(old_value) == list:
+        obj[key].append(value)
+    elif type(old_value) == tuple:
+        obj[key] += (value,)
     else:
-        if container is None:
-            obj[key] = value
-        else:
-            obj[key] = container(value)
+        obj[key] = value
 
 
 def convert_int(name, value):
@@ -56,8 +114,124 @@ type_converters={
 
 
 def convert(params, fields):
+    """convert(params, fields) -> data, errors
+
+    This function converts and validates the ``params`` tuple of (name, value)
+    pairs.  The data converted is returned in the ``data`` dict.  Errors
+    encountered during conversion are set in the ``errors`` dict.
+
+    The ``fields`` parameter is used to convert/validate the data.  It it's a
+    dict with either simple types or dicts as value, e.g::
+
+        >>> fields = {
+        ...     "user": {
+        ...         "name": str,
+        ...         "nick": str,
+        ...         "age": int,
+        ...     }
+        ... }
+
+    Using that ``schema-like`` structure, we're able to validate and convert form request
+    data::
+
+        >>> params = (("user.name", "Fred Kaputnik"), ("user.nick", "fred"),
+        ...     ("user.age", 42))
+
+    Note, the ``names`` in the name, value tuples of the params are ``path`` names.
+
+        >>> from repoze.formapi.converter import convert
+        >>> data, errors = convert(params, fields)
+
+    As all the tuples in ``params`` are valid, we get no error::
+
+        >>> errors
+        {'user': {'nick': None, 'age': None, 'name': None}}
+
+    Usually, we would request a error for a ``path``like so::
+
+        >>> from repoze.formapi.converter import resolve_name
+        >>> resolve_name("user.nick", errors) is None
+        True
+
+    Ok, the data is converted w/o error.  Let's have a look::
+
+        >>> resolve_name("user.name", data)
+        'Fred Kaputnik'
+        >>> resolve_name("user.nick", data)
+        'fred'
+        >>> resolve_name("user.age", data)
+        42
+
+    Let's provoke a validation error::
+
+        >>> data, errors = convert((("user.age", "ten"),), fields)
+        >>> resolve_name("user.age", data) is None
+        True
+        >>> resolve_name("user.age", errors)
+        'Error converting value to integer'
+
+    Note that the ``data` dict is populated with ``None`` values as a missing value::
+
+        >>> data
+        {'user': {'nick': None, 'age': None, 'name': None}}
+
+    This was simple.  Now let's consider this field structure::
+
+        >>> fields = {
+        ...     "user": {
+        ...         "name": str,
+        ...         "nick": str,
+        ...         "friends": [ str, ],
+        ...     }
+        ... }
+
+    Obviously, we want to have ``user.friends`` to be a a list of friends::
+
+        >>> params = (("user.friends", "stefan"), ("user.friends", "malthe"))
+        >>> data, errors = convert(params, fields)
+        >>> resolve_name("user.friends", data)
+        ['stefan', 'malthe']
+
+    The same goes for tuples::
+
+        >>> fields = {
+        ...     "user": {
+        ...         "name": str,
+        ...         "nick": str,
+        ...         "points": (int, ),
+        ...     }
+        ... }
+        >>> params = (("user.points", 42), ("user.points", 10))
+        >>> data, errors = convert(params, fields)
+        >>> resolve_name("user.points", data)
+        (42, 10)
+
+    Now for something more complex. consider this field structure::
+
+        >>> fields = {
+        ...     "user_list": [{
+        ...         "name": str,
+        ...         "nick": str,
+        ...     }, ]
+        ... }
+
+    Here we clearly expect a ``list of users``, e.g. a list of dicts.
+
+    Currently, we don't support that.
+
+    """
     data = dict()
     errors = dict()
+
+    # initialize data dict
+    for path in path_iterator(fields):
+        data_type = resolve_name(path, fields)
+        if type(data_type) == list:
+            store_item(path, list(), data)
+        elif type(data_type) == tuple:
+            store_item(path, tuple(), data)
+        else:
+            store_item(path, None, data)
 
     for param in params:
         name, value = param
@@ -65,10 +239,8 @@ def convert(params, fields):
         # fetch the data type
         data_type = resolve_name(name, fields)
 
-        container_type = None
         if type(data_type) in (list, tuple):
             # these merely specify the container type.  Fetch the data type
-            container_type = type(data_type)
             data_type = data_type[0]
 
         # fetch type converter
@@ -80,10 +252,10 @@ def convert(params, fields):
         value, error = converter(name, value)
 
         # store value in data dict
-        store_item(name, value, data, create_empty=True, container=container_type)
+        store_item(name, value, data)
 
         # store error in error dict
-        store_item(name, error, errors, create_empty=True, container=container_type)
+        store_item(name, error, errors)
 
     return data, errors
 
--- src/repoze/formapi/tests.py
+++ src/repoze/formapi/tests.py
@@ -16,132 +16,25 @@ class Request:
             params = {}
         self.params = params
 
-class TestDataConverter(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_store_item(self):
-        """test storing items in a dict"""
-        from repoze.formapi.converter import store_item
-
-        data = {"foo": None,
-                "bar": {
-                    "baz": None,
-                    }
-                }
-
-        store_item("foo", "value1", data)
-        store_item("bar.baz", "value2", data)
-        store_item("bar.new", "value3", data)
-
-        self.assertEqual(data["foo"], "value1")
-        self.assertEqual(data["bar"]["baz"], "value2")
-        self.assertEqual(data["bar"]["new"], "value3")
-
-    def test_store_item_raises(self):
-        """"""
-        from repoze.formapi.converter import store_item
-        self.assertRaises(KeyError, store_item, "not.there", "foo", dict())
-
-    def test_resolver_resolves(self):
-        """the resolver sould resolve names in a dict"""
-        from repoze.formapi.converter import resolve_name
-
-        data = {
-                "user": {
-                    "name": "Fred Kaputnik",
-                    "address": "Foo Street",
-                    }
-                }
-
-        self.assertEqual(resolve_name("user.name", data), "Fred Kaputnik")
-        self.assertEqual(resolve_name("user.address", data), "Foo Street")
-
-    def test_resolver_raises(self):
-        """the resolver sould raise KeyError on unknown names"""
-        from repoze.formapi.converter import resolve_name
-        self.assertRaises(KeyError, resolve_name, "not.there", dict())
-
-    def test_convert_returns_dict(self):
-        """converter should return a tuple of data, error (each a dict)"""
-
-        fields = {
-                "user": {
-                    "name": str,
-                    "address": str,
-                    }
-                }
-        params = (("user.name", "Fred Kaputnik"), ("user.address", "Foo Street"))
-
-        data, errors = convert(params, fields)
-
-        self.assertEqual(type(data), dict)
-        self.assertEqual(type(errors),dict)
-
-    def test_convert_populates_dicts(self):
-        """the converter returns a populates dicts even if no parameters given"""
-        raise NotImplementedError("nothin to see here")
-
-    def test_errors_dict_act_as_boolean(self):
-        """the error dict sould acct as boolean
-
-        That is, the error dict should be "True" if there are errors, "False"
-        otherwise.
-        """
-        raise NotImplementedError("nothin to see here")
-
-    def test_convert_converts(self):
-        """converter converting data"""
-
-        fields = {
-                "user": {
-                    "name":    str,
-                    "address": str,
-                    "dates":   [int],
-                    "tuple":   (float,),
-                    }
-                }
-        params = (("user.name", "Fred Kaputnik"),
-                  ("user.address", "Foo Street"),
-                  ("user.dates", "17"),
-                  ("user.tuple", "3.14"),
-                  ("user.dates", "43"),
-                  ("user.tuple", "0.5"),
-                  ("user.dates", "21"))
-
-        data, errors = convert(params, fields)
-
-        self.assertEqual(data["user"]["name"], "Fred Kaputnik")
-        self.assertEqual(data["user"]["address"], "Foo Street")
-        self.assertEqual(data["user"]["dates"], [17, 43, 21])
-        self.assertEqual(data["user"]["tuple"], (3.14, 0.5))
-
-    def test_type_converters(self):
-        from repoze.formapi.converter import convert_int
-        from repoze.formapi.converter import convert_float
-
-        value, error = convert_int("foo", "42")
-        self.assertEqual(value, 42)
-        self.assertEqual(error, None)
-
-        value, error = convert_int("foo", "abc")
-        self.assertEqual(value, None)
-        self.assertEqual(error, "Error converting value to integer")
 
 def test_suite():
     globs = dict(Request=Request)
     
     return unittest.TestSuite([
         doctest.DocFileSuite(
-        'README.txt',
-        optionflags=OPTIONFLAGS,
-        globs=globs,
-        package="repoze.formapi"),
+            'README.txt',
+            optionflags=OPTIONFLAGS,
+            globs=globs,
+            package="repoze.formapi"),
         doctest.DocTestSuite(
-        'repoze.formapi.form',
-        optionflags=OPTIONFLAGS,
-        globs=globs),
-        unittest.makeSuite(TestDataConverter)])
+            'repoze.formapi.form',
+            optionflags=OPTIONFLAGS,
+            globs=globs),
+        doctest.DocTestSuite(
+            'repoze.formapi.converter',
+            optionflags=OPTIONFLAGS,
+            globs=globs)
+        ])
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
